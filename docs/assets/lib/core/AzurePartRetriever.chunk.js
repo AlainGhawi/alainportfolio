5695f88b0a7a9ca164a17d780e62e2bdcba71f31
Alain Ghawi <ghawialain@gmail.com>
2023-10-25 12:52:47 -0400

    Changes to the css files

---
+++ docs/assets/lib/core/AzurePartRetriever.chunk.js
@@ -0,0 +1,3 @@
+/** Notice * This file contains works from many authors under various (but compatible) licenses. Please see core.txt for more information. **/
+(function(){(window.wpCoreControlsBundle=window.wpCoreControlsBundle||[]).push([[3],{523:function(wa,ta,n){n.r(ta);var pa=n(0),oa=n(176);wa=n(517);n=n(438);wa=function(na){function ja(ka,fa,y,x){return na.call(this,ka,fa,y,x)||this}Object(pa.c)(ja,na);ja.prototype.w0=function(){return{start:this.r_-oa.a,stop:this.r_}};ja.prototype.bw=function(ka){var fa=this;this.pq(this.url,{start:0,stop:1},function(y,x,f){if(y)return ka(y);y=f.request.getResponseHeader("Content-Range");fa.r_=y.split("/")[1];na.prototype.bw.call(fa,
+ka)})};return ja}(wa["default"]);Object(n.a)(wa);Object(n.b)(wa);ta["default"]=wa}}]);}).call(this || window)
