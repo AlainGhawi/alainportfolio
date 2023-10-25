8f3de2ae8eca729ae05556edb9ade8a6bbc886b0
Alain Ghawi <ghawialain@gmail.com>
2023-10-25 11:58:46 -0400

    Deploying the new changes for the relative path for localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 @NgModule({
   declarations: [
